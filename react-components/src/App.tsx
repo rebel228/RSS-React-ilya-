97c3eb089e2512410de84084f17e93e9b95abfcc
Ilya <rany4ka@gmail.com>
2023-10-29 15:40:41 +0300

    fix:upd to class components

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -1,7 +1,10 @@
+import React, { Component } from 'react';
 import Main from './components/main-page';
 
-function App() {
-  return <Main />;
+class App extends Component {
+  render() {
+    return <Main />;
+  }
 }
 
 export default App;
